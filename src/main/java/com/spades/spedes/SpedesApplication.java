69176fc9fbe12ba8fe23e28e67909ff4bd764b2a
Jamie Bowers <jamiebrs@gmail.com>
2019-03-16 13:38:44 -0400

    Initial Commit.

---
+++ src/main/java/com/spades/spedes/SpedesApplication.java
@@ -0,0 +1,13 @@
+package com.spades.spedes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpedesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpedesApplication.class, args);
+    }
+
+}
